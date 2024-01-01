af8812be41b2dff1b7952f1f47851bbafbdc5ec6
aaefea <ahmet.saygili@istun.edu.tr>
2024-01-01 16:48:22 +0300

    sa
    sa

---
+++ bizimkiler project/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject Sword;
+    public bool CanAttack = true;
+    public float AttackCooldown = 1f;
+    public AudioClip SwordAttackSound;
+    public bool isAttacking = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(CanAttack)
+            {
+                SwordAttack();
+            }
+        }
+    }
+
+
+    public void SwordAttack()
+    {
+        isAttacking = true;
+        CanAttack = false;
+        Animator anim = Sword.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+        AudioSource ac = GetComponent<AudioSource>();
+        ac.PlayOneShot(SwordAttackSound);
+        StartCoroutine(ResetAttackCooldown());
+    }
+
+
+    IEnumerator ResetAttackCooldown()
+    {
+        StartCoroutine(ResetAttackBool());
+        yield return new WaitForSeconds(AttackCooldown);
+        CanAttack = true;
+    }
+
+    IEnumerator ResetAttackBool()
+    {
+        yield return new WaitForSeconds(1f);
+        isAttacking = false;
+    }
+}
