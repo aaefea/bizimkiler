45a33a7dd9550b890834ee5e9aa63827df4d0964
aaefea <ahmet.saygili@istun.edu.tr>
2024-01-16 07:02:12 +0300

    a
    a

--- bizimkiler project/Assets/sonda.cs
+++ bizimkiler project/Assets/sonda.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class sonda : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject contmetni;
+    public GameObject video;
+
+ void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            video.SetActive(true);
+            StartCoroutine(dakkabekle());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+   IEnumerator dakkabekle()
     {
-        
+        yield return new WaitForSeconds(64);
+        video.SetActive(true);
+        contmetni.SetActive(true);
     }
 }
